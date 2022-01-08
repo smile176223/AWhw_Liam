96c8a855a26e6e4c0a7d770b9ac7edd6df644d99
LiamHao <smile176223@gmail.com>
2022-01-08 15:34:47 +0800

    Add Week1 Assignment: Basic

---
+++ Week1/AWShw_liam_Week1_Basic.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+let Pi = Double.pi
+
+let x:Int = 3
+let y:Int = 3
+let average = (x + y)/2
+
+let x2:Double = 3
+let y2:Double = 3
+let average2 = (x2 + y2)/2
+
+var flag:Bool = true
+var inputString:String = "Hello world."
+let bitsInBite:Int = 8
+let averageScore:Double = 86.8
+
+var salary = 22000
+salary += 28000
+
+salary == 50000
+
+let c = 10
+let d = 3
+let remainder = c % d
+
+
